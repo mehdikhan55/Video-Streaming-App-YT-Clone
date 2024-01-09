4878961a22e9efb5cc528e8cb0232ebc05e898ae
Mehdi Ali <mehdikhanofficial@gmail.com>
2024-01-09 17:24:46 +0500

    starting with some code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBQAo9HSiF0lVeA_SoF-RHonCsmDNV2Vow",
+    authDomain: "yt-clone-5bac4.firebaseapp.com",
+    projectId: "yt-clone-5bac4",
+    storageBucket: "yt-clone-5bac4.appspot.com",
+    messagingSenderId: "1085711023277",
+    appId: "1:1085711023277:web:9bf4deaf993ed8817a5a24",
+    measurementId: "G-5M28W42LCD"
+  };
